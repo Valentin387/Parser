1c5539521a719703f5a1e36dfbeebda676e19a2f
Valentin387 <valentin.valencia@utp.edu.co>
2022-11-30 14:42:09 -0500

    Point 4 successfully implemented

--- Tests/ThreePointsV1.mc
+++ Tests/ThreePointsV1.mc
@@ -1,2 +1,4 @@
-
-print(format("Format function works %d %d %d"));
+var a=10;
+var b=PI;
+var c=3;
+print(format("Format %X function works %.8f\n\n second %s line",a,b,c));
--- stdlib.py
+++ stdlib.py
@@ -5,16 +5,24 @@ class CallError(Exception):
 	pass
 
 class Format:
-    def __call__(self, interp, *args):
-        if len(args) != 1:
-            raise CallError("Error in argument in 'format' ")
-        if not isinstance(args[0], (str)):
-            raise CallError("'format' argument must be string type ")
-        return args[0]
+	def __call__(self, interp, *args):
+		print(args[1:])
+		if not isinstance(args[0], (str)):
+			raise CallError("'format' argument must be string type ")
+		if args[0].count('%') != len(args[1:]):
+			raise CallError("'format' mismatch in arguments ")
+		#string formatting operator
+		return args[0].replace('\\n','\n') % args[1:]
 
-    def __str__(self):
-        return '<builtins: Format>'
-
+	def __str__(self):
+		return '<builtins: Format>'
+"""
+%s - String (or any object with a string representation, like numbers)
+%d - Integers
+%f - Floating point numbers
+%.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+%x/%X - Integers in hex representation (lowercase/uppercase)
+"""
 class ArcTang:
     def __call__(self, interp, *args):
         if len(args) != 1:
@@ -41,5 +49,10 @@ stdlibFunctions = {
 
     'format':Format(),
     'atan':ArcTang(),
-    'log':Logarithm()
+    'log':Logarithm(),
+	'PI':math.pi,
+	'EULER':math.e,
+	'TAU':math.tau,
+	'INF':math.inf,
+	'NAN':math.nan
 }
